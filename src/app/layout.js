8590b92bb65cc6fdd14cb7bc3cb6eefe5a8227b7
Jean Mayer <jean.mayer@ae.studio>
2023-05-15 09:38:21 -0300

    maybe here?

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,59 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Octofriendly',
-  description: 'Follow all members of a GitHub organization',
-}
+  title: "Octofriendly",
+  description: "Follow all members of a GitHub organization",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>
+        Octofriendly - Automatically follow users from a Github organization
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/png" href="/logo.png" />
+      <meta
+        name="title"
+        content="Octofriendly - Automatically follow users from a Github organization"
+      />
+      <meta
+        name="description"
+        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://octofriendly.com/" />
+      <meta
+        property="og:title"
+        content="Octofriendly - Automatically follow users from a Github organization"
+      />
+      <meta
+        property="og:description"
+        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
+      />
+      <meta property="og:image" content="https://octofriendly.com/og.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://octofriendly.com/" />
+      <meta
+        property="twitter:title"
+        content="Octofriendly - Automatically follow users from a Github organization"
+      />
+      <meta
+        property="twitter:description"
+        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
+      />
+      <meta
+        property="twitter:image"
+        content="https://octofriendly.com/og.png"
+      />
       <body>{children}</body>
+      <script
+        async
+        defer
+        src="https://scripts.simpleanalyticscdn.com/latest.js"
+      ></script>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -147,46 +147,6 @@ export default function Home() {
 
   return (
     <>
-      <title>
-        Octofriendly - Automatically follow users from a Github organization
-      </title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" type="image/png" href="/logo.png" />
-      <meta
-        name="title"
-        content="Octofriendly - Automatically follow users from a Github organization"
-      />
-      <meta
-        name="description"
-        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
-      />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://octofriendly.com/" />
-      <meta
-        property="og:title"
-        content="Octofriendly - Automatically follow users from a Github organization"
-      />
-      <meta
-        property="og:description"
-        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
-      />
-      <meta property="og:image" content="https://octofriendly.com/og.png" />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://octofriendly.com/" />
-      <meta
-        property="twitter:title"
-        content="Octofriendly - Automatically follow users from a Github organization"
-      />
-      <meta
-        property="twitter:description"
-        content="Octofriendly is an app that makes it easy to find and follow users in your Github organization. Use the Github API to get up to date data and find new people to follow. Get started now!"
-      />
-      <meta
-        property="twitter:image"
-        content="https://octofriendly.com/og.png"
-      />
       <main className="flex flex-col items-center p-24 max-w-lg m-auto">
         <h1 className="text-4xl font-medium pb-8">Octofriendly</h1>
         <div className="m-0 mb-6 leading-8">
@@ -257,11 +217,6 @@ export default function Home() {
             ae.studio
           </a>
         </footer>
-        <script
-          async
-          defer
-          src="https://scripts.simpleanalyticscdn.com/latest.js"
-        ></script>
       </main>
     </>
   );
